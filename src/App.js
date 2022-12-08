868ee99034f0fa87c11ad87b10b0991777f9e5d2
Tejasri-99 <tejapamulapati99@gmail.com>
2022-12-08 16:38:44 +0530

    example of redux

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import SendingData  from'./SendingData'
+import {useSelector,useDispatch} from 'react-redux'
+import {increment,decrement} from './actions'
+
+function App(){
+ const count=useSelector(state=>state.counter)
+ const dispatch=useDispatch()
+  return(
+    <div style={{textAlign:"center"}}>
+      <h1>{count}</h1>
+      <button onClick={()=>dispatch(increment())}>+</button>    
+      <button onClick={()=>dispatch(decrement())}>-</button> 
+    </div>
+  )
+}
+export default App;
